16b2818452781f21733bcbd96107eda267ae0096
Bob Taylor <btaylor@allytechllc.com>
2024-08-08 15:30:14 -0400

    first commit

---
+++ lib/led_screen/led_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LedScreen extends StatefulWidget {
+  const LedScreen({super.key});
+
+  @override
+  State<LedScreen> createState() => _LedScreenState();
+}
+
+class _LedScreenState extends State<LedScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+//TODO: Import the led_service.dart file
+//TODO: Create an instance of the LedService class
+//TODO: Call the initGpio16Output method in the initState method
+//TODO: Call the disposeGpio method in the dispose method
+//TODO: Create the getElevatedButtonLedOn method
+//TODO: Create the getElevatedButtonLedOff method
+//TODO: Create a Scaffold widget
+//TODO: Add an AppBar widget
+//TODO: Add a Center widget
+//TODO: Add a Column widget
+//TODO: Add the getElevatedButtonLedOn method
+//TODO: Add the getElevatedButtonLedOff method
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_led_gpio/led_screen/led_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Udemy LED GPIO',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LedScreen(),
+    );
+  }
+}
